acb48527ce94054f80d6dc0edd41178c0feba5b5
DevWithEasy <devwitheasy@gmail.com>
2023-12-24 14:32:03 +0600

    api url change

--- src/renderer/src/utils/api_url.js
+++ src/renderer/src/utils/api_url.js
@@ -1,3 +1,3 @@
-const api_url = 'http://localhost:8080'
+const api_url = process.NODE_ENV === 'production' ? 'https://hospital-api-3ijx.onrender.com' : 'http://localhost:8080'
 
 export default api_url
